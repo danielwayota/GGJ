80cb76ef2aeddd24db384005de29651aa544e7d6
Víctor Gabriel Carvajal Aróstegui <victorgabrielarostegui@gmail.com>
2020-02-01 12:13:04 +0000

    Collaborator photo and grave

---
+++ Assets/Scripts/Grave.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grave : MonoBehaviour
+{
+    //Atributos.
+    private const int FOTOS = 5;
+    private int recogidas;
+
+    //Constructor.
+    private void Awake()
+    {
+
+    }
+
+    //Métodos.
+    public void FotosRecogidas()
+    {
+        this.recogidas++;
+    }
+
+    //Método que observa si hay tocamientos con la tumba.
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (this.recogidas < FOTOS)
+        {
+            Debug.Log("Debes tener todas las fotos.");
+        }
+        else
+        {
+            Debug.Log("Tienes todas las fotos.");
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/Photo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Photo : MonoBehaviour
+{
+    //Atributos.
+    private Grave grave; //Puntero a la otra clase.
+
+    //Constructor.
+    private void Awake()
+    {
+        this.grave = FindObjectOfType<Grave>();
+        if (this.grave == null)
+        {
+            Debug.LogError("NO HAY TUMBA!");
+        }
+    }
+
+    //Métodos.
+
+
+    //Método de tocamiento.
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        this.grave.FotosRecogidas(); //La foto le manda una señal a tumba.
+        this.gameObject.SetActive(false); //La foto desaparece al contacto.
+    }
+}
