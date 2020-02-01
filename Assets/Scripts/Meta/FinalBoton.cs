ca83a73f9a25edf1de9ac2a27d70523269bdc08f
Víctor Gabriel Carvajal Aróstegui <victorgabrielarostegui@gmail.com>
2020-02-01 22:38:02 +0000

    Botón y pumba

---
+++ Assets/Scripts/Meta/FinalBoton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalBoton : MonoBehaviour
+{
+    public GameObject menu;
+
+    public void BackMenu()
+    {
+        Loading.LoadScene(SceneNames.MENU);
+
+    }
+}
