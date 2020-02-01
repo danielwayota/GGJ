bfc2de35e229b2e09975a72ec585ec47d401163c
Víctor Gabriel Carvajal Aróstegui <victorgabrielarostegui@gmail.com>
2020-02-01 12:48:15 +0000

    Spikes changed

---
+++ Assets/Scripts/Spikes.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour
+{
+    //Atributos.
+
+
+
+
+    //Constructor.
+
+
+    //Métodos.
+
+    //Método al tocarse sensualmente.
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var moco = other.gameObject.GetComponent<Moco>(); //Salta al tocarse "SOLAMENTE" con el moco.
+        moco.gameObject.SetActive(false); //El moco desaparece al tocar los pinchos.
+        Debug.Log("Te has muerto");
+    }
+}
